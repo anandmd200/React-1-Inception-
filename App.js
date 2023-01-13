1720545d91925c5b89149cd7c960ae95206484b2
anandmd200 <80383762+anandmd200@users.noreply.github.com>
2023-01-13 11:25:36 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,17 @@
+const heading1 = React.createElement(
+  "h1",
+  { id: "title",},
+  "Hello React"
+);
+const heading2 = React.createElement(
+  "h1",
+  { className: "hello" },
+  "Hello React"
+);
+const container = React.createElement(
+    "div",
+     { id: "container"}, 
+     [heading1, heading2]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
